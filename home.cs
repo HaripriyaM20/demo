a8649a16abb0ce2448ea376b1922e823b2f589ef
Administrator1 <haripriyamahalingam20@gmail.com>
2022-09-14 17:52:04 +0530

    Frist Commit

---
+++ home.cs
@@ -0,0 +1,18 @@
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+int y = 6;
+int sum = x + y;
+Console.WriteLine(sum); 
+        }
+    }
+}
+
